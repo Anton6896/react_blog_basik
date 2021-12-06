73bedd88373aba094c5785f13ad1a61e7612a209
ant <anton-r@live.com>
2021-12-06 22:15:51 +0200

    added parallax

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import NavBar from "./components/navbar/NavBar";
 import Home from "./components/pages/home/Home";
+import "./index.css"
 
 
 function App() {
     return (
         <div>
             <NavBar/>
+            <div className="parallax"></div>
             <Home/>
+
+            <div className="parallax1"></div>
+
         </div>
     );
 }
